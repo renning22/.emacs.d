29170cfe663a33ecae3c136502bcc6a91e2e9198
renning22@hotmail.com <renning22@hotmail.com>
2014-05-22 17:54:56 +0800

    asdasd

--- init.el
+++ init.el
@@ -40,3 +40,20 @@
 
 (require 'smart-tab)
 (global-smart-tab-mode 1)
+
+
+;;; auto-save
+;; automatically save buffers associated with files on buffer switch
+;; and on windows switch
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
