4a0e9c8d9266ad4542e8945b8ab98d049773ff94
Ning Ren <ningr@google.com>
2014-09-28 18:05:49 +0100

    scroll as move cursor

--- init.el
+++ init.el
@@ -233,6 +233,28 @@ Repeated invocations toggle between the two most recently open buffers."
 ;;;;;;;;;;
 
 
+;;;;;;;;;; Make scrolling move cursors
+;;
+(defun sfp-page-down ()
+  (interactive)
+  (setq this-command 'next-line)
+  (next-line
+   (- (window-text-height)
+      next-screen-context-lines)))
+
+(defun sfp-page-up ()
+  (interactive)
+  (setq this-command 'previous-line)
+  (previous-line
+   (- (window-text-height)
+      next-screen-context-lines)))
+
+(global-set-key [next] 'sfp-page-down)
+(global-set-key [prior] 'sfp-page-up)
+;;
+;;;;;;;;;;
+
+
 (defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
 
 ;; ~~~~~
